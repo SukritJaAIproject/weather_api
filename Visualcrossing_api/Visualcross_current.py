007fccc18adcd022b566b27f4844ff181fed53c0
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2022-04-14 13:24:57 +0700

    Update Visualcross_current.py

--- Visualcrossing_api/Visualcross_current.py
+++ Visualcrossing_api/Visualcross_current.py
@@ -1,22 +1,27 @@
 import csv
 import json
-import codecs
-import urllib.request
 import sys
+import codecs
 import pandas as pd
+import urllib.request
+from datetime import datetime
+from datetime import date
 from google.colab import files
 
-########## historical data ##########
+########## current data ##########
 lat, lon = "13.989478999999996", "100.616387"
-start_date, end_date = "2021-01-01", "2021-01-15"
 
 # api_key = "Q82HADKP8VA46L5QNL2LJLC4Y" #Chula
 # api_key = "2DSCEBCSZCZ87NGFDKK3Z84GB" #KU
 api_key = "9ECDU5PSBATV5BF3VYZH2MRU4" #Thanksfordoo
 
-def Visualcrossing_hist(lat, lon, start_date, end_date, api_key):
+current_date = datetime.today().strftime("%Y-%m-%d")
+current_time = datetime.now().strftime("%H:%M:%S")
+print("current_date =", current_date, "Current Time =", current_time)
+
+def Visualcross_current(lat, lon, api_key, current_date, current_time):
   url1 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-  url2 = str(lat)+"%2C%20"+str(lon)+"/"+str(start_date)+"/"+str(end_date)
+  url2 = str(lat)+"%2C%20"+str(lon)+"/"+str(current_date)+"/"+str(current_date)
   url3 = "?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctemp%2Chumidity%2Cstations%2Csource&include=obs%2Chours&key="
   url4 = str(api_key)+"&contentType=json"
   url = url1+url2+url3+url4
@@ -31,23 +36,17 @@ def Visualcrossing_hist(lat, lon, start_date, end_date, api_key):
     ErrorInfo= e.read().decode() 
     print('Error code: ', e.code,ErrorInfo)
     sys.exit()
-
   his_data = jsonData['days']
-  dates, times = [], []
-  temps, rhs = [], []
+  dates, times, temps, rhs  = [], [],  [], []
   for i in range(len(his_data)):
     date, data = his_data[i]['datetime'], his_data[i]['hours']
     for j in range(len(data)):
-      datetime = data[j]['datetime']
+      datetimes = data[j]['datetime']
       temp, rh = data[j]['temp'], data[j]['humidity']
-      dates.append(date)
-      times.append(datetime)
-      temps.append(temp)
-      rhs.append(rh)
-  df = pd.DataFrame({'date':dates, 'time':times, 'temperature':temps, 'humidity':rhs})
-  df.to_csv('Visualcrossing_'+str(start_date)+'_'+str(end_date)+'.csv') 
-  # files.download('Visualcrossing_01_01_2021_15_01_2021.csv')
-  return df
-
-#df = Visualcrossing_hist(lat, lon, start_date, end_date, api_key, url)
-#df
+      if datetimes[0:2] == current_time[0:2]:
+        # print('datetime[0:2]', datetimes[0:2], 'current_time[0:2]', current_time[0:2])
+        print('datetime: ', datetimes, 'current_time: ', current_time , 'Temp: ', temp, 'humidity: ', rh)
+        result = {'datetime':datetimes, 'current_time':current_time , 'Temp':temp, 'humidity':rh}
+  return result
+result = Visualcross_current(lat, lon, api_key, current_date, current_time)
+result
