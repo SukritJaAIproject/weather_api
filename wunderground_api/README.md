6a79dc6999bd6623d759074d5c42e0eeeb8754ff
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2022-04-20 22:55:42 +0700

    upload

---
+++ wunderground_api/README.md
@@ -0,0 +1,23 @@
+# wunderground
+
+## API doc
+- https://docs.google.com/document/d/1w8jbqfAk0tfZS5P7hYnar1JiitM0gQZB-clxDfG3aD0/edit#
+- https://docs.google.com/document/d/1eKCnKXI9xnoMGRRzOL1xPCBihNV2rOet08qpE_gArAY/edit
+
+## all code
+- https://colab.research.google.com/drive/1jN9PQjwhKO2xiZcnYNwovau26v-ojEyg?usp=sharing
+
+## reference link
+- https://wunderweather.readthedocs.io/en/latest/
+- https://www.esipfed.org/blog-archive/esiplabupdate/wunderweather-python-lightning-talks-agu-google-soc-and-more
+- https://indilib.org/forum/ekos/5298-wunderground-api-issue.html
+- https://libs.garden/python/search?q=weather+underground
+- https://openthings.freshdesk.com/support/solutions/articles/5000017485-getting-a-weather-underground-wu-api-key
+- https://sandbox.idre.ucla.edu/sandbox/python-and-the-web-2018
+- https://cran.r-project.org/web/packages/rwunderground/README.html
+- https://github.com/dh4/pywu
+- https://programtalk.com/vs2/python/3890/i3pystatus/i3pystatus/weather/wunderground.py/
+- https://www.shanelynn.ie/analysis-of-weather-data-using-pandas-python-and-seaborn/
+- https://projects.raspberrypi.org/en/projects/uploading-weather-data-to-weather-underground/5
+- https://github.com/MarcoBuster/WUndergroundPWS-API
+- https://www.pythonforbeginners.com/scraping/scraping-wunderground
---
+++ wunderground_api/mean_current.py
@@ -0,0 +1,79 @@
+import json
+import tqdm
+import requests
+import pandas as pd
+from statistics import mean
+
+def location_near(lat_near, lon_near, apiKey):
+  '''หา station ที่ใกล้กับ lat lon ที่ป้อน
+  req param : lat lon
+  res param : stationName, stationId, obsType, lat, lon, distanceKm
+  '''
+  url = "https://api.weather.com/v3/location/near?geocode="+str(lat_near)+","+str(lon_near)+"&product=observation&format=json&apiKey="+str(apiKey)
+  payload, headers={}, {}
+  response = requests.request("GET", url, headers=headers, data=payload)
+  data = json.loads(response.text)
+  stationName, stationId = data['location']['stationName'], data['location']['stationId']
+  obsType, distanceKm = data['location']['obsType'], data['location']['distanceKm']
+  lat, lon = data['location']['latitude'], data['location']['longitude']
+  stationNames, stationIds, obsTypes, distanceKms, lats, lons = [], [], [],  [], [], []
+  for i in range(len(stationName)):
+    stationNames.append(stationName[i]); stationIds.append(stationId[i]); obsTypes.append(obsType[i]);
+    lats.append(lat[i]); lons.append(lon[i]); distanceKms.append(distanceKm[i]);
+  df = pd.DataFrame({'stationName':stationNames, 'stationId':stationIds, 'obsType':obsTypes, 'lat':lats, 'lon':lons, 'distanceKm':distanceKms})
+  return df
+
+def location_point(lat, lon, apiKey):
+  '''หา pwsId  จาก lat lon ที่ป้อน
+  req param : lat lon
+  res param : lat, lon, city, pwsId
+  '''
+  url = "https://api.weather.com/v3/location/point?format=json&geocode="+str(lat)+","+str(lon)+"&language=en-US&apiKey="+str(apiKey)
+  payload, headers={}, {}
+  response = requests.request("GET", url, headers=headers, data=payload)
+  data = json.loads(response.text)
+  latitude, longitude =  data['location']['latitude'], data['location']['longitude']
+  city, pwsId = data['location']['city'], data['location']['pwsId']
+  return [latitude, longitude, city, pwsId]
+
+def wunderground_current(lat, lon, apiKey):
+  ''' location near -> location point -> current data
+  req param : lat lon
+  res param : lat, lon, mean temp, mean rh
+  '''
+  lat2, lon2, city2, pwsId2, stationId2, tempav, rhav  = [], [], [], [], [],[], []
+  df1 = location_near(lat, lon, apiKey) ## location near
+  for i in range(df1.shape[0]):
+    try:
+      lat, lon, stationId = df1['lat'][i], df1['lon'][i], df1['stationId'][i]
+      info_list = location_point(lat, lon, apiKey) ## location point
+      lat2.append(info_list[0]); lon2.append(info_list[1]);
+      city2.append(info_list[2]); pwsId2.append(info_list[3]); stationId2.append(stationId);
+    except:
+      lat2.append(0); lon2.append(0);
+      city2.append("None"); pwsId2.append("None"); stationId2.append("None");
+  df2 = pd.DataFrame({'stationId':stationId2, 'lat':lat2, 'lon':lon2, 'city':city2,'pwsId':pwsId2})
+  for stationId in df2['pwsId']:
+    url = "https://api.weather.com/v2/pws/observations/current?stationId="+str(stationId)+"&format=json&units=m&apiKey="+str(apiKey)
+    payload, headers={}, {}
+    try:
+      response = requests.request("GET", url, headers=headers, data=payload)
+      data = json.loads(response.text)
+      stationID = data['observations'][0]['stationID']
+      obsTimeLocal, neighborhood = data['observations'][0]['obsTimeLocal'], data['observations'][0]['neighborhood']
+      solarRadiation = data['observations'][0]['solarRadiation']
+      lat, lon = data['observations'][0]['lat'], data['observations'][0]['lon']
+      humidity, temp = data['observations'][0]['humidity'], data['observations'][0]['metric']['temp']
+      print(stationId, 'lat:', lat, 'lon:', lon,'temp:', temp, 'humidity:', humidity)
+      tempav.append(temp); rhav.append(humidity); 
+    except:
+      pass
+  return {'lat':lat, 'lon':lon, 'mean temp':mean(tempav), 'mean rh':mean(rhav)}
+ 
+latlons = {'chula': (13.7389852,100.528069),  'chula_14z': (13.7389592,100.5274038),
+           'ChangArena_z17': (14.9658694,103.0921753), 'Futurepark':(13.989478999999996, 100.616387 )}
+
+# ChangArena_z17  
+lat, lon, apiKey =  14.9658694, 103.0921753 , "92034bae5e864c0a834bae5e86fc0a18"
+result = wunderground_current(lat, lon, apiKey)
+print(result)
