4b56680c8a2a9203ed8f6832d36f42db6820c977
SukritJaAIproject <6170306321@student.chula.ac.th>
2022-04-23 16:43:14 +0700

    uploads

--- Meteostat_api/his_data_point.py
+++ Meteostat_api/his_data_point.py
@@ -1,5 +1,4 @@
 import json
-import pytz
 import time as timesleep
 import requests
 import datetime
@@ -29,15 +28,16 @@ def current_data_point(lat, lon, start, end, api_key, cur_date, cur_time):
   '''
   url = "https://meteostat.p.rapidapi.com/point/hourly"
   querystring, headers = {"lat":lat, "lon":lon, "start":start, "end":end, "tz":"Asia/Bangkok","model":"true"}, {"X-RapidAPI-Host": "meteostat.p.rapidapi.com", "X-RapidAPI-Key": api_key}
-  timesleep.sleep(60)
+  timesleep.sleep(10)
   response = requests.request("GET", url, headers=headers, params=querystring)
   data = json.loads(response.text)
-  #print(data)
   times, temps, idx, rhs = [], [], [], []
   for i in range(len(data['data'])):
     time, temp, rh = data['data'][i]['time'], data['data'][i]['temp'], data['data'][i]['rhum']
     times.append(time); temps.append(temp); rhs.append(rh);  idx.append(i);
   df = pd.DataFrame({'idx':idx, 'datetime':times, 'temp':temps, 'rh':rhs})
+  print(df['datetime'], '_' ,df['datetime'][i][11:-6])
+  print('cur_time', cur_time)
   for i in range(df.shape[0]):
     if df['datetime'][i][11:-6] == cur_time:
       time_m2, temp, rh = df['datetime'][i], df['temp'][i], df['rh'][i]
@@ -45,17 +45,22 @@ def current_data_point(lat, lon, start, end, api_key, cur_date, cur_time):
       pass
   return time_m2, temp, rh
 
-## Testing
-# lat, lon = "13.989478999999996", "100.616387"
-# start, end = "2021-01-01", "2021-01-15"
-# #api_key = "a949fee7f6mshf15640e97c60b31p1a6549jsn36e6b0350757"
+# ## Testing
+# # lat, lon = "13.989478999999996", "100.616387"
+# # start, end = "2021-01-01", "2021-01-15"
+# # #api_key = "a949fee7f6mshf15640e97c60b31p1a6549jsn36e6b0350757"
 # api_key = "078ced9a19mshb9078983cee367dp125d20jsn8d25ea17c1eb"
 
-# cur_date, cur_time = datetime.today().strftime("%Y-%m-%d"), datetime.now().strftime("%H")
-# print("current_date =", cur_date, "Current Time =", cur_time)
+# cur_date = datetime.today().strftime("%Y-%m-%d")
+# cur_time = datetime.now(pytz.timezone('Asia/Bangkok')).strftime("%H")
+# # print("current_date =", cur_date, "Current Time =", cur_time)
 
-# df = his_data_point(lat, lon, start, end, api_key)
-# print(df)
+# # df = his_data_point(lat, lon, start, end, api_key)
+# # print(df)
 
-# temp, rh = current_data_point(lat, lon, start, end, api_key, cur_date, cur_time)
-# print(temp, rh)
+# start = datetime.today().strftime("%Y-%m-%d")
+# end = datetime.today().strftime("%Y-%m-%d")
+# # end = datetime.now(pytz.timezone('Asia/Bangkok')).strftime("%H:%M:%S")
+
+# datetime, temp, rh = current_data_point(lat, lon, start, end, api_key, cur_date, cur_time)
+# print(datetime, temp, rh)
